0fb1bacc621ff581f6e1466d070bf4257c51eb92
Marcelo Junior <Marcelo.Junior@fit-tecnologia.org.br>
2024-12-29 16:11:47 -0400

    2nd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoPageComponent } from './components/photo-page/photo-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoPageComponent
   ],
   imports: [
     BrowserModule,
